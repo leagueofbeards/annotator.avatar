4cc271b0d16f04082d08c01b0be928af1bea8fa1
Chris J. Davis <cjd@chrisjdavis.org>
2013-02-20 09:54:34 -0800

    Initial Checkin
    Current working version of the plugin. Requires a LA javascript object to be present.

---
+++ annotator.avatar.js
@@ -0,0 +1,17 @@
+Annotator.Plugin.Avatar = function (element, message) {
+  var plugin = {};
+
+	plugin.pluginInit = function () {
+		this.annotator.viewer.addField({
+			load: function (field, annotation) {
+				if( annotation.avatar ) {
+					field.innerHTML = annotation.avatar + '&nbsp;&nbsp;' + annotation.user;
+				} else {
+					field.innerHTML = LA.avatar + '&nbsp;&nbsp;' + LA.username;
+				}
+			}
+		});
+	};
+
+	return plugin;
+}
